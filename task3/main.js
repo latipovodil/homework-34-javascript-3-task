484339a31a0fb9acaeb155ccf4af54cb9fd3981f
Odilbek <113532560+odilbekl@users.noreply.github.com>
2022-11-16 14:26:34 +0500

    task 2 end

---
+++ task3/main.js
@@ -0,0 +1,18 @@
+let a = prompt("Gapni kiriting")
+
+let b = prompt("Gapni o'rtasidagi belginni kiriting");
+
+let box = '';
+
+let i = 0;
+
+while (i < a.length) {
+
+  box += a[i]
+  box += b
+
+
+  i++;
+}
+
+document.write(box);
